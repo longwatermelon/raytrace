56d6adaec179e5be881ec1d5f47ba221d7115817
longwatermelon <longwatermelon1@gmail.com>
2022-03-09 23:18:15 -0700

    camera adjustment

--- src/main.c
+++ src/main.c
@@ -11,8 +11,8 @@ int main(int argc, char **argv)
     size_t nspheres = sizeof(spheres) / sizeof(spheres[0]);
 
     Light lights[] = {
-        (Light){ (Vec3f){ 2.f, -1.f, 0.f }, 0.8f },
-        (Light){ (Vec3f){ -1.f, -5.f, 6.f }, 0.8f }
+        (Light){ (Vec3f){ 4.f, -1.f, 0.f }, 0.8f },
+        (Light){ (Vec3f){ -3.f, -5.f, 6.f }, 0.8f }
     };
 
     size_t nlights = sizeof(lights) / sizeof(lights[0]);
@@ -39,7 +39,7 @@ int main(int argc, char **argv)
         mfloor,
         mesh_alloc((Vec3f){ 2.f, 3.f, 12.f }, "res/icosphere.obj", (Vec3f){ .9f, .6f, .7f }),
         mesh_alloc((Vec3f){ -1.f, 2.2f, 18.f }, "res/donut.obj", (Vec3f){ .6f, .7f, .9f }),
-        mesh_alloc((Vec3f){ -.4f, 3.2f, 7.f }, "res/monkey.obj", (Vec3f){ .3f, .9f, .9f })
+        mesh_alloc((Vec3f){ -.4f, 3.4f, 7.f }, "res/monkey.obj", (Vec3f){ .3f, .9f, .9f })
     };
 
     size_t nmeshes = sizeof(meshes) / sizeof(meshes[0]);
