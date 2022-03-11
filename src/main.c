bbf9231e366e40d2ecf1527495be78970ee00f5f
longwatermelon <longwatermelon1@gmail.com>
2022-03-10 21:38:28 -0700

    readd floor

--- src/main.c
+++ src/main.c
@@ -19,15 +19,12 @@ int main(int argc, char **argv)
     size_t nlights = sizeof(lights) / sizeof(Light);
 
     struct Mesh *meshes[] = {
-        // mesh_alloc((Vec3f){ 0.f, 4.f, 0.f }, "res/ground.obj", (Vec3f){ .5f, .9f, .5f }),
+        mesh_alloc((Vec3f){ 0.f, 4.f, 0.f }, "res/floor.obj", (Vec3f){ .5f, .9f, .5f }),
         mesh_alloc((Vec3f){ -3.f, 3.f, 20.f }, "res/cube.obj", (Vec3f){ .7f, .95f, 1.f }),
         mesh_alloc((Vec3f){ 2.f, 3.f, 17.f }, "res/icosphere.obj", (Vec3f){ .9f, .6f, .7f }),
         mesh_alloc((Vec3f){ -1.f, 2.2f, 23.f }, "res/donut.obj", (Vec3f){ .6f, .7f, .9f }),
-        mesh_alloc((Vec3f){ -1.f, 3.4f, 12.f }, "res/monkeyt.obj", (Vec3f){ .95f, .7f, .95f }),
-        mesh_alloc((Vec3f){ -.4f, 3.4f, 16.f }, "res/cylinder.obj", (Vec3f){ .95f, .7f, .7f }),
-
-        // mesh_alloc((Vec3f){ -.4f, 3.4f, 16.f }, "res/left_ball.obj", (Vec3f){ .95f, .7f, .7f }),
-        // mesh_alloc((Vec3f){ -.4f, 3.4f, 16.f }, "res/right_ball.obj", (Vec3f){ .95f, .7f, .7f }),
+        mesh_alloc((Vec3f){ -1.f, 3.4f, 12.f }, "res/monkey.obj", (Vec3f){ .95f, .7f, .95f }),
+        mesh_alloc((Vec3f){ 3.2f, 3.f, 13.f }, "res/cylinder.obj", (Vec3f){ .95f, .7f, .7f }),
     };
 
     meshes[0]->left_rx = -1.f;
@@ -38,7 +35,7 @@ int main(int argc, char **argv)
     size_t nmeshes = sizeof(meshes) / sizeof(struct Mesh*);
 
     render_set_dim(1000, 1000);
-    // render_set_spheres(spheres, nspheres);
+    render_set_spheres(spheres, nspheres);
     render_set_lights(lights, nlights);
     render_set_meshes(meshes, nmeshes);
 
