2b8be157e215d09afe4657d3ce48e14174f5d37d
longwatermelon <longwatermelon1@gmail.com>
2022-05-14 20:04:34 -0700

    multiline for readability

--- src/animate/main.c
+++ src/animate/main.c
@@ -5,8 +5,12 @@ int main(int argc, char **argv)
 {
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window *w = SDL_CreateWindow("Animator", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-    SDL_Renderer *r = SDL_CreateRenderer(w, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    SDL_Window *w = SDL_CreateWindow("Animator",
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600,
+        SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+
+    SDL_Renderer *r = SDL_CreateRenderer(w, -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
     struct Prog *p = prog_alloc(w, r);
     prog_mainloop(p);
